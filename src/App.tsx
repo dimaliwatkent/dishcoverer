0410b9120506c67cf2df0cb1bebd19dfc80bd222
dimaliwatkent <dimaliwatkent@gmail.com>
2024-05-03 08:53:47 +0800

    add initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, ButtonGroup } from "@nextui-org/button";
+
+const App = () => {
+  return (
+    <div>
+      <div>App</div>
+      <Button color="primary" variant="flat">
+        Button
+      </Button>
+    </div>
+  );
+};
+
+export default App;
